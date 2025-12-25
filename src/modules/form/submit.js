22a90547c66e2ea454aff1999fbb2a86971a2eb3
kenny <kennedy_ken99@hotmail.com>
2025-12-25 08:44:20 -0300

    configurando a data com dayjs e capturando os dados do submit

---
+++ src/modules/form/submit.js
@@ -0,0 +1,16 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+// Date atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Carrega a data atual e Define a data mínima para agendamentos, sendo a data atual
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit =(event) => {
+  event.preventDefault()
+  console.log("enviado")
+}
