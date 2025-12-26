0184d83b5137a5700578a4fcc674664ae3954acf
kenny <kennedy_ken99@hotmail.com>
2025-12-26 20:06:43 -0300

    Separando os horários pelo período

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -26,6 +26,24 @@ export function hoursLoad({ date }) {
     li.classList.add(avaiable ? "hour-available" : "hour-unavailable")
 
     li.textContent = hour
+
+    if(hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "19:00") {
+      hourHeaderAdd("Noite")
+    }
+
     hours.appendChild(li)
   })
+}
+
+function hourHeaderAdd(title){
+  const header = document.createElement("li")
+
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
